5de6d5bfee5556393c278e8b4817e779a543a33d
Matvey <72647958+FuckingGook@users.noreply.github.com>
2020-10-31 16:07:52 +0700

    Update LAB1_FizzBuzz_1.c

--- LAB1_FizzBuzz_1.c
+++ LAB1_FizzBuzz_1.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main() 
-{
-  for( int i = 1; i<100; i++ )
+int main()
+{ 
+  for (int i = 1; i <= 100; i++)
   {
-    if( i % 5 == 0 && i % 3 == 0 )
+    if ((i % 3 == 0) && (i % 5 != 0))
+    {
+      printf("Fizz\n");
+    }
+    if ((i % 3 != 0) && (i % 5 == 0))
+    {
+      printf("Buzz\n");
+    }
+    if ((i % 3 == 0) && (i % 5 == 0))
     {
       printf("FizzBuzz\n");
     }
-    else
+    if ((i % 3 != 0) && (i % 5 != 0))
     {
-      if( i % 5 == 0 )
-      {
-        printf("Buzz\n"); 
-      }
-      else
-      {
-        if( i % 3 == 0 )
-        {
-          printf("Fizz\n");
-        }
-        else
-        {
-          printf("%d\n", i);
-        }
-      }
+      printf("%d\n", i);
     }
   }
-  getchar();
-  getchar();
   
   return 0;
 }
