f68fe119a515bde70493113ffb30ff0460c24c77
Matvey <72647958+FuckingGook@users.noreply.github.com>
2020-10-31 16:08:52 +0700

    Update LAB1_Функция возведения в степень_3.c

--- LAB1_Функция возведения в степень_3.c
+++ LAB1_Функция возведения в степень_3.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <conio.h>
 #include <math.h>
 
-int power(int number, int power); 
-
-int main()
+int is_simple(long int number) 
 {
-  int N = 0, m = 0;
-  printf("enter number: ");
-  scanf("%d", &N);
-  printf("enter power: ");
-  scanf("%d", &m);
-  printf("%.0d", power(N, m) );
-  getchar();
-  getchar();
-
-  return 0;
-} 
-
-int power(int number, int power)
-{ 
-  float rez = 1;
-  for( int i = 0; i < power; i++)
+  if (number > 1)
   {
-    rez *= number;
+    for(int i = 2; i < number; i++)
+    {
+      if (number % i == 0)
+      {
+        return 0;
+      }
+      
+      return 1;
+    }
   }
+  else
+  {
+    printf("the_number_you_entered_is_neither_composite_nor_simple. \n");
+  }
+  
+  return 0;
+}
 
-  return rez;
+int main(void)
+{
+  long int number;
+  printf("enter_the_number: ");
+  scanf("%li", &number);
+  printf("%d", is_simple(number));
 }
